80a845cc970cb67cfb31cd915f085c246816d50c
Ayanga Oluwamurewa <phoenixflower2005@gmail.com>
2023-08-24 10:59:20 +0100

    Committing modal project

---
+++ modal-project/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <div>
+    <button class="openModal" @click="handleClick">Click Me!</button>
+    <button class="openModal" @click="toggleModal">open modal</button>
+    <button class="openModal" @click="toggleModal2">open modal 2</button>
+  </div>
+  <Modal v-if="displayModal" @close="toggleModal"/>
+  <Modal v-if="displayModal2" @close="toggleModal2">
+    I'm better than Modal 1
+  </Modal>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue"
+
+export default {
+  data() {
+    return {
+      title: "My First Vue App :)",
+      displayModal: false,
+      displayModal2: false
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$refs.name.focus()
+    },
+    toggleModal() {
+      this.displayModal = !this.displayModal
+    },
+    toggleModal2() {
+      this.displayModal2 = !this.displayModal2
+    }
+  },
+  components: {
+    Modal
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.openModal {
+  display: inline-block;
+  background-color: transparent;
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid black;
+  border-radius: 5px;
+}
+</style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal">
+             <slot>Cool Modal</slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        closeModal() {
+            this.$emit("close");
+        }
+    }
+}
+</script>
+
+<style>
+    .modal {
+        height: 100px;
+        width: 300px;
+        background-color: white;
+        border-radius: 5px;
+        padding: 20px;
+        margin: 120px auto;
+    }
+
+    .backdrop {
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+</style>
